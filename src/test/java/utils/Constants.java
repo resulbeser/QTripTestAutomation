819a6570210bc238b44bd156cd9b3ad3a9271075
Resul Beser <93214337+resulbeser@users.noreply.github.com>
2025-10-16 00:08:53 +0300

    Refactored project structure: removed unused components and streamlined test architecture for clarity and maintainability

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Constants {
+    public static final class ErrorMessages {
+        public static final String EMAIL_EXISTS = "Email already exists";
+        public static final String PASSWORD_SHORT = "Failed - Password must be atleast 6 in length";
+    }
+
+    public static final class TestData {
+        public static final String REGISTER_DATA_FILE = "registerData.json";
+    }
+}
